7d1c10fdc7ee08ce21f9c94dd5ee08100ef8eac2
sinisaos <sinisaos@gmail.com>
2024-11-16 09:32:48 +0100

    additional Piccolo Admin configuration

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /code
 
 COPY ./requirements.txt /code/requirements.txt
 
+COPY ./config.yaml /code/config.yaml
+
 RUN pip install --no-cache-dir -r /code/requirements.txt
 
 COPY ./app /code/app
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 
+import yaml
 from hypercorn import Config
 from hypercorn.asyncio import serve
 from piccolo.apps.user.tables import BaseUser
@@ -8,13 +9,20 @@ from piccolo.engine import PostgresEngine
 from piccolo.engine.sqlite import SQLiteEngine
 from piccolo.table import create_db_tables
 from piccolo.table_reflection import TableStorage
-from piccolo_admin import create_admin
+from piccolo_admin.endpoints import TableConfig, create_admin
 from piccolo_api.encryption.providers import XChaCha20Provider
 from piccolo_api.mfa.authenticator.provider import AuthenticatorProvider
 from piccolo_api.mfa.authenticator.tables import (
     AuthenticatorSecret as AuthenticatorSecret_,
 )
 from piccolo_api.session_auth.tables import SessionsBase
+
+with open("config.yaml") as stream:
+    try:
+        admin_config = yaml.safe_load(stream)
+        BASE_CONFIG = admin_config["tables"]
+    except yaml.YAMLError as exc:
+        raise exc
 
 DB = SQLiteEngine()
 
@@ -60,32 +68,52 @@ async def main():
         extensions=tuple(),
     )
 
-    tables_to_show = [
-        table_to_show.lower().strip()
-        for table_to_show in os.environ["TABLES_TO_SHOW"].split(",")
-    ]
-
     storage = TableStorage(engine=db)
     await storage.reflect(schema_name="public")
 
-    # tables to show in admin
-    if len(tables_to_show) == 1:
-        found_tables = storage.tables.values()
-    else:
+    # additional configuration of admin tables
+    if BASE_CONFIG is not None:
+        tables_to_show = [table.lower() for table in BASE_CONFIG]
         found_tables = [
             table
             for table in storage.tables.values()
             if table._meta.tablename in tables_to_show
         ]
+        admin_tables = []
+        for table in found_tables:
+            capitalize_table_name = table._meta.tablename.capitalize()
+            admin_tables.append(
+                TableConfig(
+                    table_class=table,
+                    visible_columns=[
+                        column
+                        for column in table._meta.columns
+                        if column._meta.name
+                        in BASE_CONFIG[capitalize_table_name]["visible_columns"]
+                    ],
+                    visible_filters=[
+                        column
+                        for column in table._meta.columns
+                        if column._meta.name
+                        in BASE_CONFIG[capitalize_table_name]["visible_filters"]
+                    ],
+                    menu_group=BASE_CONFIG[capitalize_table_name]["menu_group"],
+                )
+            )
+    else:
+        admin_tables = storage.tables.values()
 
-    for table_class in found_tables:
-        table_class._meta._db = db
+    for table in admin_tables:
+        if isinstance(table, TableConfig):
+            table.table_class._meta._db = db
+        else:
+            table._meta._db = db
 
     # create new encription key for MFA
     encryption_key = XChaCha20Provider.get_new_key()
 
     app = create_admin(
-        found_tables,
+        admin_tables,
         auth_table=User,
         session_table=Sessions,
         auto_include_related=False,
